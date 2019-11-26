6a9c7aec32f205f0192342abc914cb663bda3b4b
12345678 <alan>
2019-11-26 10:35:11 +0800

    将动态菜单和路由的加载放置到路由导航守卫beforeEach函数

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,9 @@ import Login from '@/views/Login'
 import NotFound from '@/views/Error/404'
 import Home from '@/views/Home'
 import Intro from '@/views/Intro/Intro'
+import api from '@/http/api'
+import store from '@/store'
+import { isURL } from '@/utils/validate'
 
 Vue.use(Router)
 
@@ -33,22 +36,91 @@ const router = new Router({
 router.beforeEach((to, from, next) => {
   // 登录界面登录成功之后，会把用户信息保存在会话
   // 存在时间为会话生命周期，页面关闭即失效。
-  let user = sessionStorage.getItem('user');
-  if (to.path == '/login') {
+  let isLogin = sessionStorage.getItem('user')
+  if (to.path === '/login') {
     // 如果是访问登录界面，如果用户会话信息存在，代表已登录过，跳转到主页
-    if(user) {
+    if(isLogin) {
       next({ path: '/' })
     } else {
       next()
     }
   } else {
     // 如果访问非登录界面，且户会话信息不存在，代表未登录，则跳转到登录界面
-    if (!user) {
+    if (!isLogin) {
       next({ path: '/login' })
     } else {
+      // 加载动态菜单和路由
+      addDynamicMenuAndRoutes()
       next()
     }
   }
 })
+
+/**
+* 加载动态菜单和路由
+*/
+function addDynamicMenuAndRoutes() {
+  api.menu.findMenuTree()
+  .then( (res) => {
+    store.commit('setMenuTree', res.data)
+    // 添加动态路由
+    let dynamicRoutes = addDynamicRoutes(res.data)
+    router.options.routes[0].children = router.options.routes[0].children.concat(dynamicRoutes)
+    router.addRoutes(router.options.routes);
+  })
+  .catch(function(res) {
+    alert(res);
+  });
+}
+
+/**
+* 添加动态(菜单)路由
+* @param {*} menuList 菜单列表
+* @param {*} routes 递归创建的动态(菜单)路由
+*/
+function addDynamicRoutes (menuList = [], routes = []) {
+ var temp = []
+ for (var i = 0; i < menuList.length; i++) {
+   if (menuList[i].children && menuList[i].children.length >= 1) {
+     temp = temp.concat(menuList[i].children)
+   } else if (menuList[i].url && /\S/.test(menuList[i].url)) {
+     menuList[i].url = menuList[i].url.replace(/^\//, '')
+     // 创建路由配置
+     var route = {
+       path: menuList[i].url,
+       component: null,
+       name: menuList[i].name,
+       meta: {
+         menuId: menuList[i].menuId,
+         title: menuList[i].name,
+         isDynamic: true,
+         isTab: true,
+         iframeUrl: ''
+       }
+     }
+     // url以http[s]://开头, 通过iframe展示
+     if (isURL(menuList[i].url)) {
+       route['path'] = menuList[i].url
+       route['name'] = menuList[i].name
+       route['meta']['iframeUrl'] = menuList[i].url
+     } else {
+       try {
+         // 根据菜单URL动态加载vue组件，这里要求vue组件须按照url路径存储
+         // 如url="sys/user"，则组件路径应是"@/views/sys/user.vue",否则组件加载不到
+         let array = menuList[i].url.split('/')
+         let url = array[0].substring(0,1).toUpperCase()+array[0].substring(1) + '/' + array[1].substring(0,1).toUpperCase()+array[1]  .substring(1)
+         route['component'] = resolve => require([`@/views/${url}`], resolve)
+       } catch (e) {}
+     }
+     routes.push(route)
+   }
+ }
+ if (temp.length >= 1) {
+   addDynamicRoutes(temp, routes)
+ } else {
+   console.log(routes)
+ }
+ return routes
+}
 
 export default router
--- src/views/MenuBar/MenuBar.vue
+++ src/views/MenuBar/MenuBar.vue
@@ -15,86 +15,20 @@
 
 <script>
 import { mapState } from 'vuex'
-import { isURL } from '@/utils/validate'
-import MenuTree from "@/components/MenuTree/index"
+import MenuTree from "@/components/MenuTree"
 export default {
   components:{
         MenuTree
   },
   methods: {
     handleopen() {
-      console.log('handleopen');
+      console.log('handleopen')
     },
     handleclose() {
-      console.log('handleclose');
+      console.log('handleclose')
     },
     handleselect(a, b) {
-      console.log('handleselect');
-    },
-    /**
-     * 添加动态(菜单)路由
-     * @param {*} menuList 菜单列表
-     * @param {*} routes 递归创建的动态(菜单)路由
-     */
-    addDynamicMenuRoutes (menuList = [], routes = []) {
-      var temp = []
-      for (var i = 0; i < menuList.length; i++) {
-        if (menuList[i].children && menuList[i].children.length >= 1) {
-          temp = temp.concat(menuList[i].children)
-        } else if (menuList[i].url && /\S/.test(menuList[i].url)) {
-          menuList[i].url = menuList[i].url.replace(/^\//, '')
-          // 创建路由配置
-          var route = {
-            path: menuList[i].url,
-            component: null,
-            name: menuList[i].name,
-            meta: {
-              menuId: menuList[i].menuId,
-              title: menuList[i].name,
-              isDynamic: true,
-              isTab: true,
-              iframeUrl: ''
-            }
-          }
-          // url以http[s]://开头, 通过iframe展示
-          if (isURL(menuList[i].url)) {
-            route['path'] = menuList[i].url
-            route['name'] = menuList[i].name
-            route['meta']['iframeUrl'] = menuList[i].url
-          } else {
-            try {
-              // 根据菜单URL动态加载vue组件，这里要求vue组件须按照url路径存储
-              // 如url="sys/user"，则组件路径应是"@/views/sys/user.vue",否则组件加载不到
-              let array = menuList[i].url.split('/')
-              let url = array[0].substring(0,1).toUpperCase()+array[0].substring(1) + '/' + array[1].substring(0,1).toUpperCase()+array[1]  .substring(1)
-              route['component'] = resolve => require([`@/views/${url}`], resolve)
-            } catch (e) {}
-          }
-          routes.push(route)
-        }
-      }
-      if (temp.length >= 1) {
-        this.addDynamicMenuRoutes(temp, routes)
-      } else {
-        console.log(routes)
-      }
-      return routes
-    },
-    // 加载导航菜单
-    findMenuTree() {
-      this.$api.menu.findMenuTree()
-      .then( (res) => {
-        this.$store.commit('setMenuTree', res.data)
-        // 添加动态路由
-        let routes = this.addDynamicMenuRoutes(res.data)
-        for(var i = 0; i < routes.length; i++) {
-          this.$router.options.routes[0].children.push(routes[i])
-        }
-        this.$router.addRoutes(this.$router.options.routes);
-      })
-      .catch(function(res) {
-        alert(res);
-      });
+      console.log('handleselect')
     }
   },
   computed: {
@@ -104,11 +38,8 @@ export default {
       collapse: state=>state.app.collapse,
       menuTree: state=>state.menu.menuTree
     })
-  },
-  mounted: function() {
-    this.findMenuTree()
   }
-};
+}
 </script>
 
 <style scoped lang="scss">
